e726ba3a30d603dbca6eb657bf4149d1eeb5d751
shankar <shankargajula53@gmail.com>
2022-07-19 11:04:53 +0530

    changes

---
+++ vars/create_tag.groovy
@@ -0,0 +1,10 @@
+def create_tag(String tag) {
+	    
+	 sh """    
+	    git log -1
+	    git tag -a ${tag} -m "this tag has been created from jenkins"
+	    git push origin ${tag}    
+	   """
+	}
+	
+	return this
---
+++ vars/deploy_tomcat.groovy
@@ -0,0 +1,11 @@
+def deploy_tomcat(String tag, String env) {
+    
+    echo "deploying to tomcat server."
+   sh """
+      cd ${WORKSPACE}/shellscripts
+      sh ./deploy_java.sh ${tag} ${env}
+      """      
+
+}
+
+return this
