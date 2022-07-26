bdc2ca0b92db137891a5754d652457bf91c1e765
shankar <shankargajula53@gmail.com>
2022-07-26 13:32:10 +0530

    d

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -7,9 +7,9 @@ mkdir /var/lib/jenkins/practice
 cd /var/lib/jenkins/practice
 aws s3 cp s3://jenkins-test-8/java_code8/$tag/wwp-1.0.0.war .
 scp wwp-1.0.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
- ssh ec2-user@${IP} << `HERE'
+ ssh ec2-user@${IP} << `'HERE'
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.64/bin/
     ./shutdown.sh
     ./startup.sh
-`HERE'        
+`'HERE'        
