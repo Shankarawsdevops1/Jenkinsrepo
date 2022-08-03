3c8e14ff03edd5d9ee566b828fb63cf34eafe20a
shankar <shankargajula53@gmail.com>
2022-08-03 11:12:56 +0530

    f

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -6,8 +6,8 @@ IP=`grep $env env.properties`
 
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
-aws s3 cp s3://jenkins-test-8/java_code9/$tag/hello-1.0.war .
-scp  hello-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
+aws s3 cp s3://jenkins-test-8/java_code9/$tag/wwp-1.0.0.war .
+scp  wwp-1.0.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
 ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.64/bin/
