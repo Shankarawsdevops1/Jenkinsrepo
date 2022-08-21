1da7714d0b3530880d9516314f438a8c7144d77c
shankar <shankargajula53@gmail.com>
2022-08-21 12:23:43 +0530

    ss

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -6,11 +6,11 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
-aws s3 cp s3://jenkins-test-8/java_code9/$tag/hello-world-0.0.1-SNAPSHOT.war .
-scp  hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.64/webapps
+aws s3 cp s3://bucket-8/java-code1/$tag/hello-world-0.0.1-SNAPSHOT.war .
+scp  hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.65/webapps
 ssh ec2-user@${IP} << HERE
-    rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
-    cd /home/ec2-user/tomcat/apache-tomcat-9.0.64/bin/
+    rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.65/work
+    cd /home/ec2-user/tomcat/apache-tomcat-9.0.65/bin/
     ./shutdown.sh
     ./startup.sh
 HERE
