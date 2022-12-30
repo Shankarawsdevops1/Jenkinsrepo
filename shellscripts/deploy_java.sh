0c38f3f58144e439b542113fb4e2ecffd2e0b101
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2022-12-30 12:51:14 +0530

    new changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,14 +1,16 @@
  #!/bin/bash
 tag=$1
- env=$2
- IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
- mkdir /var/lib/jenkins/deploy
+env=$2
+
+IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
+
+mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
 aws s3 cp s3://shankar-gajula/java-code-1/$tag/hello-world-0.0.1-SNAPSHOT.war .
-scp  hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/webapps
+scp hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/webapps
 ssh ec2-user@${IP} << HERE
-    rm -rf /home/ec2-user/tomcat/work
-    cd /home/ec2-user/tomcat/bin/
+    rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.64/work
+    cd /home/ec2-user/tomcat/apache-tomcat-9.0.64/bin/
     ./shutdown.sh
-    ./startup.sh  
+    ./startup.sh
 HERE
