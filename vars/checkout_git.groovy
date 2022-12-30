efe67cd46af74688ba0c4dfbd91f60e1084f2416
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2022-12-30 12:03:33 +0530

    new changes

--- vars/checkout_git.groovy
+++ vars/checkout_git.groovy
@@ -1,7 +1,7 @@
 def checkout_git(String repo, String branch) {
     
     echo "this is checking out git code."
-    git branch: 'main', credentialsId: 'shankar', url: "git@github.com:Shankargoud1/${repo}.git", branch: "${branch}" 
+    git credentialsId: 'shankar', url: "git@github.com:Shankargoud1/${repo}.git", branch: "${branch}" 
  }
 
 return this
