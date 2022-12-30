b81a35caee4f4a3de05bc1f270d61bb8dadbaeb3
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2022-12-30 14:46:15 +0530

    new

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -6,9 +6,9 @@ mkdir /var/lib/jenkins/workspace
 cd /var/lib/jenkins/workspace
 aws s3 cp s3://shankar-gajula/java-code-1/$tag/hello-world-0.0.1-SNAPSHOT.war .
 scp hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/apache-tomcat-9.0.70/webapps
-ssh ec2-user@${IP}  << HERE
+ssh ec2-user@${IP}  << `HERE
     rm -rf /home/ec2-user/apache-tomcat-9.0.70/work
     cd /home/ec2-user/apache-tomcat-9.0.70/bin/
     ./shutdown.sh
     ./startup.sh
-HERE
+HERE'
