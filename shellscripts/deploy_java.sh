ec6a05da3ce1c5e09a88ed54c288f6f59d1d102c
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-01-02 15:38:40 +0530

    new

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -2,13 +2,13 @@
 tag=$1
 env=$2
 
-IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
+  IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 
-mkdir /var/lib/jenkins/deploy
-cd /var/lib/jenkins/deploy
-aws s3 cp s3://shankar-gajula/java-code-2/$tag/hello-world-0.0.1-SNAPSHOT.war .
-scp hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.70/webapps
-ssh ec2-user@${IP} << HERE
+ mkdir /var/lib/jenkins/deploy
+ cd /var/lib/jenkins/deploy
+ aws s3 cp s3://shankar-gajula/java-code-2/$tag/hello-world-0.0.1-SNAPSHOT.war .
+ scp hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.70/webapps
+ ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.70/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.70/bin/
     ./shutdown.sh
