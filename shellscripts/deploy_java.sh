ebc8a7b16ac536b708017fead30fcb7a54a72388
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-01-03 10:29:10 +0530

    new chnages

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+  #!/bin/bash
 tag=$1
 env=$2
+
 IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
-mkdir /var/lib/jenkins/anu
-cd /var/lib/jenkins/anu
-aws s3 cp s3://shankar-gajula/$tag/java-code-4/sparkjava-hello-world-1.0.war .
-scp sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.70/webapps
+
+mkdir /var/lib/jenkins/deploy
+cd /var/lib/jenkins/deploy
+aws s3 cp s3://shankar-gajula/java-code-3/$tag/sparkjava-hello-world-1.0.war .
+scp  sparkjava-hello-world-1.0.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.70/webapps
 ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.70/work
     cd /home/ec2-user/tomcat/apache-tomcat-9.0.70/bin/
     ./shutdown.sh
     ./startup.sh
-HERE 
+HERE
