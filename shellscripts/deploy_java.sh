5892ed1e61322bd8c34d5274d8ca3f48b9ce1f8c
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-01-09 17:07:16 +0530

    IP Changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -2,6 +2,7 @@
 tag=$1
 env=$2
 IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
+echo $IP
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
 aws s3 cp s3://shankar-gajula/$tag/java-code-2/hello-world-0.0.1-SNAPSHOT.war .
