1849504fa974f5cd68f90856a197dd8bdbf27c66
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-01-09 19:04:20 +0530

    changes

--- vars/deploy_tomcat.groovy
+++ vars/deploy_tomcat.groovy
@@ -3,7 +3,7 @@ def deploy_tomcat(String tag, String env) {
     echo "deploying to tomcat server."
    sh """
       cd ${WORKSPACE}/shellscripts
-      /bin/bash ./deploy_java.sh ${tag} ${env}
+      ./deploy_java.sh ${tag} ${env}
       """      
 
 }
