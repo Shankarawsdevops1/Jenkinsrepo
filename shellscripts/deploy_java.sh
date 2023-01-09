bb82368da97f345fa49f30b8ceadd74dd1c72b19
Shankargoud1 <121604196+Shankargoud1@users.noreply.github.com>
2023-01-09 18:55:25 +0530

    changes

--- shellscripts/deploy_java.sh
+++ shellscripts/deploy_java.sh
@@ -5,7 +5,8 @@ IP=`grep $env env.properties | cut -d ':' -f2 |tr -d "[:space:]"`
 echo $IP
 mkdir /var/lib/jenkins/deploy
 cd /var/lib/jenkins/deploy
-aws s3 cp s3://shankar-gajula/$tag/java-code-2/hello-world-0.0.1-SNAPSHOT.war .
+aws s3 ls
+aws s3 cp s3://shankar-gajula/${tag}/java-code-2/hello-world-0.0.1-SNAPSHOT.war .
 scp  hello-world-0.0.1-SNAPSHOT.war ec2-user@${IP}:/home/ec2-user/tomcat/apache-tomcat-9.0.70/webapps
 ssh ec2-user@${IP} << HERE
     rm -rf /home/ec2-user/tomcat/apache-tomcat-9.0.70/work
